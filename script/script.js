42d5ae338313047db80140a7825dbe627ba17087
andptrvc <44471388+andptrvc@users.noreply.github.com>
2019-11-09 13:21:56 +0100

    Finished
    TANK APP landing page

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $("#carousel").slick({
+      infinite: true,
+      dots: true,
+      dotsClass:'slick-dots',
+      useCss: true,
+      useTransform: true
+   });
+   });
+
